b80089003ed949853f5342a2ee43bccce045307e
hen6003 <henrysmith6003@gmail.com>
2019-11-10 21:12:57 +0000

    Add files via upload

--- Calculator/calc.py
+++ Calculator/calc.py
@@ -1,80 +1,86 @@
-def is_number(s):
-    try:
-        float(s)
-        return True
-    except ValueError:
-        return False
+import os
 
-def calc():
+def calculator():
     
-    print ("\033[92m\nType in a number")
-    numb1 = input("\033[0m\033[1m")
-    print ("\033[92mType in a number")
-    numb2 = input("\033[0m\033[1m")
-    print ("\033[92mWhat sort of sum do you want?\n1 = add\n2 = minus\n3 = times\n4 = divison")
-    typ = input("sum = \033[0m\033[1m")
-    
-    otr = ""
+    def is_number(s):
+        try:
+            float(s)
+            return True
+        except ValueError:
+            return False
 
-    if is_number(numb1) == False or is_number(numb2) == False:
+    def calc():
+        
+        print ("\033[92m\nType in a number")
+        numb1 = input("\033[0m\033[1m")
+        print ("\033[92mType in a number")
+        numb2 = input("\033[0m\033[1m")
+        print ("\033[92mWhat sort of sum do you want?\n1 = add\n2 = minus\n3 = times\n4 = divison")
+        typ = input("sum = \033[0m\033[1m")
+        
+        otr = ""
 
-        otr = "ERROR: is not a valid input"
+        if is_number(numb1) == False or is_number(numb2) == False:
 
-    elif typ == "1":
-        
-        numb3 = float(numb1) + float(numb2)
-        
-    elif typ == "2":
-        
-        numb3 = float(numb1) - float(numb2)
-        
-    elif typ == "3":
-        
-        numb3 = float(numb1) * float(numb2)
-        
-    elif ((typ == "4") and (numb2 != "0")):
-        
-        numb3 = float(numb1) / float(numb2)
-    
-    elif typ == "4":
+            otr = "ERROR: is not a valid input"
 
-        otr = "\033[92m\033[1mYou answer is \033[4m∞"
-        file  = open("/home/hs/Documents/Calculator/output", "w")
-        file.write("Output = ∞") 
-        file.close()
+        elif typ == "1":
+            
+            numb3 = float(numb1) + float(numb2)
+            
+        elif typ == "2":
+            
+            numb3 = float(numb1) - float(numb2)
+            
+        elif typ == "3":
+            
+            numb3 = float(numb1) * float(numb2)
+            
+        elif ((typ == "4") and (numb2 != "0")):
+            
+            numb3 = float(numb1) / float(numb2)
         
-    else:
-        
-        otr = "ERROR: is not a valid input"
-        
-    if otr == "":
+        elif typ == "4":
 
-        print ("\033[92m\033[1m\nYou answer is \033[4m" + str(f"{numb3:g}"))
-        file  = open("/home/hs/Documents/Calculator/output", "w")
-        file.write("Output = " + str(numb3)) 
-        file.close()
-    
-    else:
+            otr = "\033[92m\033[1mYou answer is \033[4m∞"
+            file  = open(os.path.dirname(os.path.abspath(__file__)) + "/calc_output", "w")
+            file.write("Output = ∞") 
+            file.close()
+            
+        else:
+            
+            otr = "ERROR: is not a valid input"
+            
+        if otr == "":
 
-        print("\033[91m\n" + otr)
-
-    print ("\033[0m\033[92m\033[1m\nPress ENTER to do more sums or any button then ENTER to exit")
-    re = input("\033[0m\033[1m")
-    
-    if re == "":
+            print ("\033[92m\033[1m\nYou answer is \033[4m" + str(f"{numb3:g}"))
+            file  = open(os.path.dirname(os.path.abspath(__file__)) + "/calc_output", "w")
+            file.write("Output = " + str(numb3)) 
+            file.close()
         
-        return True
-        
-    else:
-        
-        print("\033[92mThanks for using!")
-        return False
-        
-print("\033[0m\033[1m\033[92mWelcome to my Calculator!")
+        else:
 
-keepGoing = True
+            print("\033[91m\n" + otr)
 
-while(keepGoing):
+        print ("\033[0m\033[92m\033[1m\nPress ENTER to do more sums or any button then ENTER to exit")
+        re = input("\033[0m\033[1m")
+        
+        if re == "":
+            
+            return True
+            
+        else:
+            
+            print("\033[92mThanks for using!")
+            return False
 
-    keepGoing = calc()
+    print("\033[0m\033[1m\033[92mWelcome to my Calculator!")
+
+    keepGoing = True
+
+    while(keepGoing):
+
+        keepGoing = calc()
+
+calculator()
 
