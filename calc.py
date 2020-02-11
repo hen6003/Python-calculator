8ab471fab29588629b34d2b339d0ec503f698ca9
hen6003 <henrysmith6003@gmail.com>
2020-02-11 16:45:20 +0000

    Update calc.py

--- calc.py
+++ calc.py
@@ -1,7 +1,6 @@
 import os
 
 def calculator():
-    
     def is_number(s):
         try:
             float(s)
@@ -10,7 +9,6 @@ def calculator():
             return False
 
     def calc():
-        
         print("\033[92m\nType in a number")
         numb1 = input("\033[0m\033[1m")
         print("\033[92mType in a number")
@@ -21,56 +19,45 @@ def calculator():
         otr = ""
 
         if is_number(numb1) == False or is_number(numb2) == False:
-
             otr = "ERROR: is not a valid input"
 
         elif typ == "1":
-            
             numb3 = float(numb1) + float(numb2)
             
         elif typ == "2":
-            
             numb3 = float(numb1) - float(numb2)
             
         elif typ == "3":
-            
             numb3 = float(numb1) * float(numb2)
             
         elif ((typ == "4") and (numb2 != "0")):
-            
             numb3 = float(numb1) / float(numb2)
         
         elif typ == "4":
-
             otr = "\033[92m\033[1mYou answer is \033[4m∞"
             file  = open(os.path.dirname(os.path.abspath(__file__)) + "/calc_output", "w")
             file.write("Output = ∞") 
             file.close()
             
         else:
-            
             otr = "ERROR: is not a valid input"
             
         if otr == "":
-
-            print ("\033[92m\033[1m\nYou answer is \033[4m" + str(f"{numb3:g}"))
+            print("\033[92m\033[1m\nYou answer is \033[4m" + str(f"{numb3:g}"))
             file  = open(os.path.dirname(os.path.abspath(__file__)) + "/calc_output", "w")
             file.write("Output = " + str(numb3)) 
             file.close()
         
         else:
-
             print("\033[91m\n" + otr)
 
         print ("\033[0m\033[92m\033[1m\nPress ENTER to do more sums or any button then ENTER to exit")
         re = input("\033[0m\033[1m")
         
         if re == "":
-            
             return True
             
         else:
-            
             print("\033[92mThanks for using!")
             return False
 
@@ -79,7 +66,6 @@ def calculator():
     keepGoing = True
 
     while(keepGoing):
-
         keepGoing = calc()
 
 calculator()
